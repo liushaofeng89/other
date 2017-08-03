14349b92134ea8b5915baf754fa6d8b3477a03c9
liushaofeng <liushaofeng89@qq.com>
2017-08-04 02:28:14 +0800

    更新配置

--- JDCommentCrawler/src/cn/liushaofeng/DBConf.java
+++ JDCommentCrawler/src/cn/liushaofeng/DBConf.java
@@ -5,6 +5,6 @@ public class DBConf
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/pd-competition?characterEncoding=utf-8";
 
-    static final String USER = "root";
-    static final String PASS = "Huawei@345";
+    static final String USER = "****";
+    static final String PASS = "****";
 }
